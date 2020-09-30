8e6294d204824c226c71247aa87648e5ab77a5b5
Riteek <riteek2000@gmail.com>
2020-09-30 17:33:57 +0530

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '3670781196288270',
+        clientSecret: '3aea4716d0fb72ed39ab3413fefddb1f'
+    }
 }
